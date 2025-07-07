fe8c274b4cc7036431a36f23594a1658d158c5eb
Mauricio <mgp.94@hotmail.com>
2025-07-07 17:12:37 -0500

    Inicializamos dependencias Three.js

---
+++ galleryReact/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import './index.css'; // Mantenemos la importación del CSS global
+
+function App() {
+  return (
+    <div className="app-container">
+      {/* Aquí irán nuestras secciones y componentes */}
+      <h1>Mi Portafolio Next-Level</h1>
+    </div>
+  );
+}
+
+export default App;
