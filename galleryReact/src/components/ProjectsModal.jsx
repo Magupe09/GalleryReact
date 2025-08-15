cd75d9ee16c8b6457840f2e038cad16a6e9aa9a8
Mauricio <mgp.94@hotmail.com>
2025-08-15 14:02:38 -0500

    Mejoro los colores bases

--- galleryReact/src/components/ProjectsModal.jsx
+++ galleryReact/src/components/ProjectsModal.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from './ProjectsModal.module.css';
+import { a } from 'framer-motion/client';
+import img2 from '../assets/img/Hormigon.PNG'
+import img1 from '../assets/img/Pizzeria.PNG'
+import img3 from '../assets/img/TiendaTech.PNG'
 
 // Datos de proyectos de ejemplo
 const projects = [
-  { id: 1, title: 'Project: Gemini', year: '2023', poster: 'https://placehold.co/400x600/1a1a1a/cccccc?text=Project: Gemini' },
-  { id: 2, title: 'Quantum Leap', year: '2022', poster: 'https://placehold.co/400x600/1a1a1a/cccccc?text=Quantum+Leap' },
-  { id: 3, title: 'Web Weaver', year: '2021', poster: 'https://placehold.co/400x600/1a1a1a/cccccc?text=Web+Weaver' },
+  { id: 1, title: 'Pizzeria', year: '2023', poster: img1,url:'https://magupe09.github.io/SintenedorReact-2/' },
+  { id: 2, title: 'Quantum Leap', year: '2022', poster: img3,url:'https://magupe09.github.io/PagePielDurazno/' },
+  { id: 3, title: 'Web Weaver', year: '2021', poster: img2,url:'https://magupe09.github.io/Hormigon.E/' },
   { id: 4, title: 'Infinite Scroll', year: '2020', poster: 'https://placehold.co/400x600/1a1a1a/cccccc?text=Infinite+Scroll' },
   { id: 5, title: 'Pixel Perfect', year: '2019', poster: 'https://placehold.co/400x600/1a1a1a/cccccc?text=Pixel+Perfect' },
   { id: 6, title: 'Code Odyssey', year: '2018', poster: 'https://placehold.co/400x600/1a1a1a/cccccc?text=Code+Odyssey' },
@@ -27,6 +31,7 @@ function ProjectsModal({ onClose }) {
         <h2 className={styles.modalTitle}>Proyectos</h2>
         <div className={styles.projectsGrid}>
           {projects.map((project) => (
+            <a href={project.url}>
             <div key={project.id} className={styles.projectPoster}>
               <img src={project.poster} alt={`Poster de ${project.title}`} className={styles.posterImage} />
               <div className={styles.posterOverlay}>
@@ -34,6 +39,7 @@ function ProjectsModal({ onClose }) {
                 <p className={styles.posterYear}>{project.year}</p>
               </div>
             </div>
+            </a>
           ))}
         </div>
       </div>
